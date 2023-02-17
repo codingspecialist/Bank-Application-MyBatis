80e292e9df870685cee68021698b4270fbc43313
getinthere <getinthere@naver.com>
2023-02-17 14:38:16 +0900

    계좌상세보기 완료

--- src/main/java/shop/mtcoding/bankapp/model/history/HistoryRepository.java
+++ src/main/java/shop/mtcoding/bankapp/model/history/HistoryRepository.java
@@ -3,9 +3,13 @@ package shop.mtcoding.bankapp.model.history;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import shop.mtcoding.bankapp.dto.history.HistoryRespDto;
 
 @Mapper
 public interface HistoryRepository {
+    public List<HistoryRespDto> findByGubun(@Param("gubun") String gubun, @Param("accountId") int accountId);
 
     public int insert(History history);
 
